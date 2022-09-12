5690d50ab86fd8716921f53c555b48856074659c
Guido Lippolis <guidolippolis2012@libero.it>
2022-09-12 15:57:50 +0200

    Added constant CHAT_SECTION_HTML_ID in constants.py

--- whatsapp/constants.py
+++ whatsapp/constants.py
@@ -9,3 +9,4 @@ PATH_DRIVER_CHROME = "C:/GitHub_Repositories/WhatsApp_Scraper/drivers"
 BASE_URL = "https://web.whatsapp.com/"
 CHAT_LIST_CONTAINER = '//*[@id="pane-side"]/div[2]/div/div'
 ARCHIVED_CHATS_BUTTON = '//*[@id="pane-side"]/button/div/div[2]/div/div'
+CHAT_SECTION_HTML_ID = 'pane-side'

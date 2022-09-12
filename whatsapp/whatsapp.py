865587522308a56d241e6ac86165fe66e9c2abc2
Guido Lippolis <guidolippolis2012@libero.it>
2022-09-12 11:53:40 +0200

    Removed unused imports

--- whatsapp/whatsapp.py
+++ whatsapp/whatsapp.py
@@ -12,9 +12,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-
-from selenium.common.exceptions import NoSuchElementException
 
 from whatsapp.constants import PATH_DRIVER_CHROME
 from whatsapp.constants import BASE_URL

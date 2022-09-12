e6ba38a45fdba0f8c0111d2c0e99f7ea32789401
Guido Lippolis <guidolippolis2012@libero.it>
2022-09-12 19:48:41 +0200

    Added the method getChatOfContact(); Added the constant CHAT_MESSAGES_CONTAINER in constants.py

--- whatsapp/constants.py
+++ whatsapp/constants.py
@@ -7,6 +7,13 @@ Created on Sun Sep  4 12:10:48 2022
 
 PATH_DRIVER_CHROME = "C:/GitHub_Repositories/WhatsApp_Scraper/drivers"
 BASE_URL = "https://web.whatsapp.com/"
+
+# Contenitore principale delle chat aperte nella home
 CHAT_LIST_CONTAINER = '//*[@id="pane-side"]/div[2]/div/div'
+
+# Contenitore della chat
+CHAT_MESSAGES_CONTAINER = '//*[@id="main"]/div[3]/div/div[2]/div[3]'
+
+
 ARCHIVED_CHATS_BUTTON = '//*[@id="pane-side"]/button/div/div[2]/div/div'
 CHAT_SECTION_HTML_ID = 'pane-side'
--- whatsapp/whatsapp.py
+++ whatsapp/whatsapp.py
@@ -17,6 +17,7 @@ from whatsapp.constants import BASE_URL
 from whatsapp.constants import CHAT_LIST_CONTAINER
 from whatsapp.constants import ARCHIVED_CHATS_BUTTON
 from whatsapp.constants import CHAT_SECTION_HTML_ID
+from whatsapp.constants import CHAT_MESSAGES_CONTAINER
 
 class Whatsapp(webdriver.Chrome):
     
@@ -34,8 +35,9 @@ class Whatsapp(webdriver.Chrome):
     # Stops the program for the given number of seconds
     def wait(self, seconds):
         for i in range(0,seconds):
-            print(str(i) + '... \n')
+            # print(str(i) + '... \n')
             time.sleep(1)
+
 
     # Opens the browser and does all the rest
     def landFirstPage(self):
@@ -83,6 +85,8 @@ class Whatsapp(webdriver.Chrome):
                 break
         print(namesBeforeScrolling)
 
+
+
     def fillNameList(self, spanList):
         nameList = []
         self.implicitly_wait(200)
@@ -103,3 +107,72 @@ class Whatsapp(webdriver.Chrome):
         WebDriverWait(self, seconds).until(
             EC.presence_of_element_located((By.XPATH, XPath))
         )
+        
+    
+    def searchContactToClick(self, contacts, contactToSearch):
+        for contact in contacts:
+            self.wait(2)
+            name = contact.get_attribute('title')
+            if(len(name) != 0):
+                if(name == contactToSearch):
+                    contact.click()
+                    return True
+    
+        
+    def getChatOfContact(self):
+        contactName = 'Cosimo Gennaro'
+        
+        pixels = 0
+        pre_height = 0
+        new_height = 0           
+        
+        self.get(BASE_URL)
+        self.waitForElementToAppear(500, ARCHIVED_CHATS_BUTTON)
+        
+        self.wait(5)
+        chats = self.getContacts()
+        
+        contactFound = self.searchContactToClick(chats, contactName)
+        
+        if(contactFound == True):
+            print('Contatto trovato senza scrollare \n')
+        
+        endAll = False
+        
+        if(contactFound == False):
+            while True:
+                pixels += 500
+                time.sleep(0.5)
+                self.execute_script('document.getElementById("' + CHAT_SECTION_HTML_ID + '").scrollTo(0,' + str(pixels) + ')')
+                new_height = self.execute_script('return document.getElementById("' + CHAT_SECTION_HTML_ID + '").scrollTop')
+            
+                while True:
+    
+                    try:
+                        self.implicitly_wait(200)
+                        self.wait(5)
+                        scrolledChats = self.getContacts()
+                        
+                        contactFoundInScrolledChats = self.searchContactToClick(scrolledChats, contactName)
+                        
+                        if(contactFoundInScrolledChats):
+                            print('Contatto trovato \n')
+                            endAll = True
+                            break
+                        
+                        namesAfterScrolling = self.fillNameList(scrolledChats)
+                        
+                        break
+                            
+                    except:
+                        self.implicitly_wait(0.5)
+            
+                if(endAll):
+                    break
+            
+                if(pre_height < new_height):
+                    pre_height = self.execute_script('return document.getElementById("' + CHAT_SECTION_HTML_ID + '").scrollTop')
+                else:
+                    break
+        
+        

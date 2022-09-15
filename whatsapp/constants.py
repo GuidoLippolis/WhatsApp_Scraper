2ee8eb3c7a429203f76d7a8c5d18f3d893e582f7
Guido Lippolis <guidolippolis2012@libero.it>
2022-09-15 16:20:29 +0200

    Re-sorted constants.

--- whatsapp/constants.py
+++ whatsapp/constants.py
@@ -8,12 +8,14 @@ Created on Sun Sep  4 12:10:48 2022
 PATH_DRIVER_CHROME = "C:/GitHub_Repositories/WhatsApp_Scraper/drivers"
 BASE_URL = "https://web.whatsapp.com/"
 
-# Contenitore principale delle chat aperte nella home
+# Container of all open chats in the home page
 CHAT_LIST_CONTAINER = '//*[@id="pane-side"]/div[2]/div/div'
 
-# Contenitore della chat
+# Container of all messages of a single chat
 CHAT_MESSAGES_CONTAINER = '/html/body/div[1]/div/div/div[4]/div/div[3]/div/div[2]/div[3]'
 
-
-ARCHIVED_CHATS_BUTTON = '//*[@id="pane-side"]/button/div/div[2]/div/div'
+# HTML id value of CHAT_MESSAGES_CONTAINER
 CHAT_SECTION_HTML_ID = 'pane-side'
+
+# Button to get archived chats
+ARCHIVED_CHATS_BUTTON = '//*[@id="pane-side"]/button/div/div[2]/div/div'

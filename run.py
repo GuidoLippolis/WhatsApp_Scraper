1a111d035bf76240dd090f2a3a26b16d988695f9
Guido Lippolis <guidolippolis2012@libero.it>
2022-10-03 12:59:19 +0200

    Added GUI

--- run.py
+++ run.py
@@ -1,15 +1,132 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep  4 10:34:17 2022
+Created on Mon Oct  3 12:16:25 2022
 
 @author: guido
 """
 
 from whatsapp.whatsapp import Whatsapp
-from app import App
 
-# app = App()
-# app.mainloop()
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog
+import threading
 
-inst = Whatsapp()
-inst.findChatToScrap()
+it = ['Data gg/mm/aaaa', 'Ora', 'Mittente', 'Messaggio', 'scraper pronto',
+      'Autori: Guido Lippolis, Cosimo Gennaro e Simone Marchitelli', 'Opzioni',
+      'Caricare lista contatti', 'Avvia scraper', 'Scraping chat archiviate', 'Cartella di destinazione']
+en = ['Date mm/dd/yyyy', 'Time', 'Sender', 'Message', 'scraper ready',
+      'Authors: Guido Lippolis, Cosimo Gennaro and Simone Marchitelli', 'Options',
+      'Load contact list', 'Start scraper', 'Scraping archived chats', 'Destination folder']
+
+def changeLanguage(index, value, op):
+    if comboLang.get() == 'English':
+        tree.heading(0, text=en[0], anchor=tk.W)
+        tree.heading(1, text=en[1], anchor=tk.W)
+        tree.heading(2, text=en[2], anchor=tk.W)
+        tree.heading(3, text=en[3], anchor=tk.W)
+        output_label_2.config(text=en[4])
+        credit_label.config(text=en[5])
+        label.config(text=en[6])
+        choose_1.config(text=en[7])
+        choose_2.config(text=en[8])
+        c2.config(text=en[9])
+        choose_dest.config(text=en[10])
+    else:
+        tree.heading(0, text=it[0], anchor=tk.W)
+        tree.heading(1, text=it[1], anchor=tk.W)
+        tree.heading(2, text=it[2], anchor=tk.W)
+        tree.heading(3, text=it[3], anchor=tk.W)
+        output_label_2.config(text=it[4])
+        credit_label.config(text=it[5])
+        label.config(text=it[6])
+        choose_1.config(text=it[7])
+        choose_2.config(text=it[8])
+        c2.config(text=it[9])
+        choose_dest.config(text=it[10])
+    return
+
+window = tk.Tk()
+window.geometry("900x625")
+window.title("WhatsApp Scraper")
+window.grid_columnconfigure(0, weight=1)
+window.resizable(False, False)
+
+tree = ttk.Treeview(window, show="headings", columns=(it[0], it[1], it[2], it[3]), height=14)
+tree.heading(it[0], text=it[0], anchor=tk.W)
+tree.heading(it[1], text=it[1], anchor=tk.W)
+tree.heading(it[2], text=it[2], anchor=tk.W)
+tree.heading(it[3], text=it[3], anchor=tk.W)
+tree.column('#1', minwidth=110, stretch=False, width=110)
+tree.column('#2', minwidth=90, stretch=False, width=90)
+tree.column('#3', minwidth=140, stretch=False, width=140)
+tree.column('#4', minwidth=535, stretch=True, width=535)
+style = ttk.Style(window)
+tree.grid(row=5, column=0, padx=10, pady=10, stick='W')
+
+vsbar = tk.Scrollbar(window, orient=tk.VERTICAL, command=tree.yview)
+vsbar.place(x=868, y=279, height=280, width=20)
+tree.configure(yscrollcommand=vsbar.set)
+
+style.theme_use("clam")
+style.configure("Treeview", background="white",
+                fieldbackground="white", foreground="white")
+tree.bind("<Button-1>", "disableEvent")
+
+title = tk.Label(window, text="WhatsApp Scraper", font=("Helvetica", 24))
+title.grid(row=0, column=0, sticky="N", padx=20, pady=10)
+
+output_label = tk.Label(text="Log: ")
+output_label.grid(row=6, column=0, sticky="W", padx=10, pady=10)
+
+output_label_2 = tk.Label(text=it[4], bg="white", fg="black", borderwidth=2, relief="groove", anchor='w')
+output_label_2.configure(width=50)
+output_label_2.grid(row=6, column=0, sticky="W", padx=45, pady=10)
+
+credit_label = tk.Label(window, text=it[5])
+credit_label.grid(row=6, column=0, stick="E", padx=10, pady=0)
+
+xf = tk.Frame(window, relief=tk.GROOVE, borderwidth=2, width=920, height=70)
+xf.grid(row=1, column=0, sticky="W", padx=10, pady=10)
+
+label = tk.Label(xf, text=it[6])
+label.place(relx=.06, rely=0.04, anchor=tk.W)
+
+choose_1 = tk.Button(text=it[7])
+choose_1.grid(row=1, column=0, sticky="W", padx=30, pady=10)
+
+xf_2 = tk.Frame(window, relief=tk.GROOVE, borderwidth=2, width=920, height=70)
+xf_2.grid(row=2, column=0, sticky="W", padx=10, pady=10)
+
+choose_dest_label = tk.Label(text="", bg="white", fg="black", borderwidth=2, relief="groove", anchor='w')
+choose_dest_label.configure(width=55)
+choose_dest_label.grid(row=2, column=0, sticky="W", padx=185, pady=10)
+
+choose_dest = tk.Button(text=it[10])
+choose_dest.grid(row=2, column=0, sticky="W", padx=30, pady=10)
+
+choose_label = tk.Label(text="", bg="white", fg="black", borderwidth=2, relief="groove", anchor='w')
+choose_label.configure(width=55)
+choose_label.grid(row=1, column=0, sticky="W", padx=185, pady=10)
+
+choose_2 = tk.Button(text=it[8])
+choose_2.grid(row=2, column=0, sticky="E", padx=30, pady=10)
+
+save_media = tk.IntVar()
+c1 = tk.Checkbutton(window, text='Scraping media', variable=save_media, onvalue=1, offvalue=0)
+c1.grid(row=1, column=0, stick="E", padx=200, pady=10)
+
+archiviate = tk.IntVar()
+c2 = tk.Checkbutton(window, text=it[9], variable=archiviate, onvalue=1, offvalue=0)
+c2.grid(row=1, column=0, stick="E", padx=30, pady=10)
+
+v = tk.StringVar()
+v.trace('w', changeLanguage)
+comboLang = ttk.Combobox(window, textvar=v, state="readonly",
+                         values=[
+                             "English",
+                             "Italian"])
+comboLang.grid(row=0, column=0, sticky="W", padx=10, pady=10)
+comboLang.set('Italian')
+
+window.mainloop()
